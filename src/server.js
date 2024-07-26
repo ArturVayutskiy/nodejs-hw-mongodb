8ae42ae121a62d133d2a81e3984581059591b2b8
ArturVayutskiy <vayutskiy@gmail.com>
2024-07-27 01:08:27 +0300

    done

---
+++ src/server.js
@@ -0,0 +1,54 @@
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+
+import { getAllcontacts, getContactById } from './services/contacts.js';
+
+const PORT = process.env.PORT || 3000;
+
+export const setupServer = () => {
+  const app = express();
+
+  app.use(express.json());
+  app.use(cors());
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.get('/contacts', async (req, res) => {
+    const contacts = await getAllcontacts();
+
+    res.status(200).json({
+      data: contacts,
+      message: 'Successfully found contacts!',
+    });
+  });
+
+  app.get('/contacts/:contactId', async (req, res, next) => {
+    const { contactId } = req.params;
+    const contact = await getContactById(contactId);
+
+    // Відповідь, якщо контакт не знайдено
+    if (!contact) {
+      res.status(404).json({
+        message: 'Contact not found',
+      });
+      return;
+    }
+
+    // Відповідь, якщо контакт знайдено
+    res.status(200).json({
+      message: `"Successfully found contact with id ${contactId}!"`,
+      data: contact,
+    });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
